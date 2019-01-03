13d33ed70828b88022db8202142fd1f248412c7f
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2019-01-03 10:27:23 +0000

    creating different route handlers

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
+
 //set up express app
 const app = express();
 
-//this is the address that receives get requests (/)
-app.get('/', function(req,res){
-    console.log('GET request')
-    res.send({ name: 'Yoshi'});
-})
 
+
+//initial routes
+app.use('/api', require('./routes/api'));
 //listen for requests
 app.listen(4000, function(){
     console.log('now listening for requests');
---
+++ routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+//get a list of ninjas from the database
+router.get('/ninjas', function(req,res){
+    res.send({type:'GET'});
+});
+
+//add a new ninja to the db
+router.post('/ninjas', function(req,res){
+    res.send({type:'POST'});
+});
+
+//update a ninja already in the db
+router.pull('/ninjas/:id', function(req,res){
+    res.send({type:'PULL'});
+});
+
+//delete a ninja from the db
+router.delete('/ninjas/:id', function(req,res){
+    res.send({type:'DELETE'});
+});
+
+module.exports = router;
