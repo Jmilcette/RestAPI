7b22bcad0433b5ccf8816b590003d18f7b295d84
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2019-01-03 10:14:35 +0000

    creating GET requests and the corresponding address

--- node.js
+++ node.js
@@ -3,6 +3,12 @@ const express = require('express');
 //set up express app
 const app = express();
 
+//this is the address that receives get requests (/)
+app.get('/', function(req,res){
+    console.log('GET request')
+    res.send({ name: 'Yoshi'});
+})
+
 //listen for requests
 app.listen(4000, function(){
     console.log('now listening for requests');
